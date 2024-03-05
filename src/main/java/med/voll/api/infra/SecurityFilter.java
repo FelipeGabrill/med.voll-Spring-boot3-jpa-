a69436b79f054abb612a2bb3375fd350d7925c73
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-03-05 00:06:40 -0300

    Testando o controle de acesso

--- src/main/java/med/voll/api/infra/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/SecurityFilter.java
@@ -25,7 +25,6 @@ public class SecurityFilter extends OncePerRequestFilter {
 	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-		
 		var tokenJWT = recuperarToken(request);
 		
 		if (tokenJWT != null) {
@@ -33,7 +32,6 @@ public class SecurityFilter extends OncePerRequestFilter {
 			var usuario = repository.findByLogin(subject);
 			
 			var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
-			
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
 		
@@ -43,7 +41,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 	private String recuperarToken(HttpServletRequest request) {
 		var authorizationHeader = request.getHeader("Authorization");
 		if (authorizationHeader != null) {
-			return authorizationHeader.replace("Bearer", "");
+			return authorizationHeader.replace("Bearer", "").trim();
 		}
 		return null;
 	}	
