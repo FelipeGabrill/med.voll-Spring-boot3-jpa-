245291f9eec1a0c7af9707f7670adfdc5a554752
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-03-06 21:12:44 -0300

    Validação de integridade

--- src/main/java/med/voll/api/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/consulta/AgendaDeConsultas.java
@@ -3,6 +3,7 @@ package med.voll.api.consulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import med.voll.api.infra.ValidacaoException;
 import med.voll.api.medico.MedicoRepository;
 import med.voll.api.pacientes.PacienteRepository;
 
@@ -19,11 +20,18 @@ public class AgendaDeConsultas {
 	private PacienteRepository pacienteRepository;
 	
 	public void agendar(DadosAgendamentoConsulta dados) {
+		if (!pacienteRepository.existsById(dados.idPaciente())) {
+			throw new ValidacaoException("Id do paciente informado não existe");
+		}
+		
+		if (dados.idMedico()!= null && !medicoRepository.existsById(dados.idMedico())) {
+			throw new ValidacaoException("Id do médico informado não existe");
+		}
 		
 		var paciente = pacienteRepository.findById(dados.idPaciente()).get();
 		var medico = medicoRepository.findById(dados.idMedico()).get();
 		var consulta = new Consulta(null, medico, paciente, dados.data());
 		consultaRepository.save(consulta);
 	}
-	
+
 }
